826afe82c822c44b852114ba0c19e3bcab372971
aioverg <1010998682@qq.com>
2019-11-22 23:19:50 +0800

    建立基础架构

---
+++ src/components/header/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2>进销存管理系统</h2>
+</template>
+
+
+
+<script>
+export default {
+    name: "header"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="login">
+        <div>
+            <p>hello</p>
+            <p><span>账号：</span><input></p>
+            <p><span>密码：</span><input></p>
+            <button>登陆</button>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+  name: 'login',
+  props: {
+  }
+}
+</script>
+<style>
+    #login div{
+        width: 320px;
+        margin: 0 auto;
+        padding: 18px 28px 28px 28px;
+        background-color: #fff;
+        border-radius: 5px;
+        box-shadow: -4px 7px 46px 2px rgba(0,0,0,.1);
+    }
+    #login span{
+        display: inline-block;
+        width: 70px;
+        text-align: center;
+    }
+    #login input{
+        height: 24px;
+    }
+    #login p:nth-child(1){
+        font-size: 38px;
+        font-weight: 600;
+        color: #1790fe;
+    }
+</style>
