0da48e466ef0773145c28ff369cd23821c6bb224
aioverg <1010998682@qq.com>
2019-11-27 21:53:46 +0800

    修改路由以适应动态路由

--- src/permission.js
+++ src/permission.js
@@ -1,6 +1,44 @@
-import router from './router'
+//import asyncRouter from './router'
 
-router.beforeEach((to, from, next) => {
-    if(1===1)
-    
-})
+/*
+export const asyncRouter = [
+    //{
+    //    path: '/main',
+    //    name: 'main',
+    //    component: () => import('./components/main'),  //主页面组件
+    //    children: [
+            {
+                path: '/system/warehousemanagement',
+                name: 'warehouseManagement',
+                component: () => import('./components/content/system/warehouseManagement'),  //仓库管理
+                //meta: { power:true }
+            },
+            {
+                path: '/system/commoditymanagement',
+                name: 'commodityManagement',
+                component: () => import('./components/content/system/commodityManagement'),  //商品管理
+                //meta: { power:true }
+            },
+            {
+                path: '/system/suppliermanagement',
+                name: 'supplierManagement',
+                component: () => import('./components/content/system/supplierManagement'),  //供应管理
+                //meta: { power:true },
+            },
+            {
+                path: '/system/customermanagement',
+                name: 'customerManagement',
+                component: () => import('./components/content/system/customerManagement'),  //客户管理
+                //meta: { power:true }
+            },
+            {
+                path: '/system/privilegemanagement',
+                name: 'privilegeManagement',
+                component: () => import('./components/content/system/privilegeManagement'),  //权限管理
+                //meta: { power:true }
+            }
+    //    ]
+    //}
+]
+*/
+//router.addRoutes(asyncRouter)
--- src/router/index.js
+++ src/router/index.js
@@ -1,154 +1,140 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import login from '../components/login'  //登录页面组件
-import main from '../components/main'    //主页面组件
-import purchase from '../components/content/purchase/purchase'  //商品采购
-import purchaseRturn from '../components/content/purchasereturn/purchaseReturn'  //采购退货
-import sell from '../components/content/sell/sell'  //商品销售
-import sellReturn from '../components/content/sellreturn/sellReturn'  //销售退货
-import inventory from '../components/content/inventory/inventory'  //库存盘点
-import contrast from '../components/content/analysis/contrast'  //进销对比
-import procurementStatistics from '../components/content/analysis/procurementStatistics'  //采购统计
-import salesStatistics from '../components/content/analysis/salesStatistics'  //销售统计
-import grossProfit from '../components/content/settlement/grossProfit'  //毛利统计
-import customerCollection from '../components/content/settlement/customerCollection' //客户收款
-import customerReconciliation from '../components/content/settlement/customerReconciliation'  //客户对账
-import supplierReconciliation from '../components/content/settlement/supplierReconciliation'  //供应商对账
-import supplierRemittance from '../components/content/settlement/supplierRemittance'  //供应商汇款
-import warehouseManagement from '../components/content/system/warehouseManagement'  //仓库管理
-import commodityManagement from '../components/content/system/commodityManagement'  //商品管理
-import supplierManagement from '../components/content/system/supplierManagement'  //供应管理
-import customerManagement from '../components/content/system/customerManagement'  //客户管理
-import privilegeManagement from '../components/content/system/privilegeManagement'  //权限管理
 
 Vue.use(Router)
 
 //通用路由
-export const constantRouter = {
-    mode: 'hash', //使用hash模式
-    routes: [
-        {  //登录
-            path: '/',
-            name: 'login',
-            component: login
-        },
-        {  //主页面
-            path: '/main',
-            name: 'main',
-            component: main,
-            children: [
-                {
-                    path: '/main',
-                    name: 'purchase',
-                    component: purchase
-                },
-                {
-                    path: '/purchasreturn',
-                    name: 'purchaseRturn',
-                    component: purchaseRturn
-                },
-                {
-                    path: '/sell',
-                    name: 'sell',
-                    component: sell
-                },
-                {
-                    path: '/sellreturn',
-                    name: 'sellReturn',
-                    component: sellReturn
-                },
-                {
-                    path: '/inventory',
-                    name: 'inventory',
-                    component: inventory
-                },
-                {
-                    path: '/analysis/contrast',
-                    name: 'contrast',
-                    component: contrast
-                },
-                {
-                    path: '/analysis/procurementstatistics',
-                    name: 'procurementStatistics',
-                    component: procurementStatistics
-                },
-                {
-                    path: '/analysis/salesstatistics',
-                    name: 'salesStatistics',
-                    component: salesStatistics
-                },
-                {
-                    path: '/settlement/customercollection',
-                    name: 'customerCollection',
-                    component: customerCollection
-                },
-                {
-                    path: '/settlement/customerreconciliation',
-                    name: 'customerReconciliation',
-                    component: customerReconciliation
-                },
-                {
-                    path: '/settlement/grossprofit',
-                    name: 'grossProfit',
-                    component: grossProfit
-                },
-                {
-                    path: '/settlement/supplierreconciliation',
-                    name: 'supplierReconciliation',
-                    component: supplierReconciliation
-                },
-                {
-                    path: '/settlement/supplierremittance',
-                    name: 'supplierRemittance',
-                    component: supplierRemittance
-                },
-            ]
-        }
-    ]
-}
+export const constantRouter = [
+    {  //登录
+        path: '/',
+        name: 'login',
+        component: () => import('../components/login')  //登录页面组件
+    },
+    {  //主页面
+        path: '/main',
+        name: 'main',
+        component: () => import('../components/main'),  //主页面组件
+        children: [
+            {
+                path: '/main',
+                name: 'purchase',
+                component: () => import('../components/content/purchase/purchase')  //商品采购
+            },
+            {
+                path: '/purchasreturn',
+                name: 'purchaseRturn',
+                component: () => import('../components/content/purchasereturn/purchaseReturn')  //采购退货
+            },
+            {
+                path: '/sell',
+                name: 'sell',
+                component: () => import('../components/content/sell/sell')  ////商品销售
+            },
+            {
+                path: '/sellreturn',
+                name: 'sellReturn',
+                component: () => import('../components/content/sellreturn/sellReturn')  //销售退货
+            },
+            {
+                path: '/inventory',
+                name: 'inventory',
+                component: () => import('../components/content/inventory/inventory')  //库存盘点
+            },
+            {
+                path: '/analysis/contrast',
+                name: 'contrast',
+                component: () => import('../components/content/analysis/contrast')  //进销对比
+            },
+            {
+                path: '/analysis/procurementstatistics',
+                name: 'procurementStatistics',
+                component: () => import('../components/content/analysis/procurementStatistics')  //采购统计
+            },
+            {
+                path: '/analysis/salesstatistics',
+                name: 'salesStatistics',
+                component: () => import('../components/content/analysis/salesStatistics')  //销售统计
+            },
+            {
+                path: '/settlement/customercollection',
+                name: 'customerCollection',
+                component: () => import('../components/content/settlement/customerCollection')  //客户收款
+            },
+            {
+                path: '/settlement/customerreconciliation',
+                name: 'customerReconciliation',
+                component: () => import('../components/content/settlement/customerReconciliation')  //客户对账
+            },
+            {
+                path: '/settlement/grossprofit',
+                name: 'grossProfit',
+                component: () => import('../components/content/settlement/grossProfit')  //毛利统计
+            },
+            {
+                path: '/settlement/supplierreconciliation',
+                name: 'supplierReconciliation',
+                component: () => import('../components/content/settlement/supplierReconciliation')  //供应商对账
+            },
+            {
+                path: '/settlement/supplierremittance',
+                name: 'supplierRemittance',
+                component: () => import('../components/content/settlement/supplierRemittance')  //供应商汇款
+            },
+        ]
+    }
+]
 
 //需要权限的路由，异步路由
-export const asyncRouter={
-    mode: 'hash',
-    routes: [
-        {
-            path: '/main',
-            name: 'main',
-            component: main,
-            children:[
-                {
-                    path: '/system/warehousemanagement',
-                    name: 'warehouseManagement',
-                    component: warehouseManagement,
-                    meta: { power:true }
-                },
-                {
-                    path: '/system/commoditymanagement',
-                    name: 'commodityManagement',
-                    component: commodityManagement,
-                    meta: { power:true }
-                },
-                {
-                    path: '/system/suppliermanagement',
-                    name: 'supplierManagement',
-                    component: supplierManagement,
-                    meta: { power:true },
-                },
-                {
-                    path: '/system/customermanagement',
-                    name: 'customerManagement',
-                    component: customerManagement,
-                    meta: { power:true }
-                },
-                {
-                    path: '/system/privilegemanagement',
-                    name: 'privilegeManagement',
-                    component: privilegeManagement,
-                    meta: { power:true }
-                }
-            ]
-        }
-    ]
-}
+export const asyncRouter = [
+    //{
+    //    path: '/main',
+    //    name: 'main',
+    //    component: () => import('../components/main'),  //主页面组件
+    //    children: [
+            {
+                path: '/system/warehousemanagement',
+                name: 'warehouseManagement',
+                component: () => import('../components/content/system/warehouseManagement'),  //仓库管理
+                //meta: { power:true }
+            },
+            {
+                path: '/system/commoditymanagement',
+                name: 'commodityManagement',
+                component: () => import('../components/content/system/commodityManagement'),  //商品管理
+                //meta: { power:true }
+            },
+            {
+                path: '/system/suppliermanagement',
+                name: 'supplierManagement',
+                component: () => import('../components/content/system/supplierManagement'),  //供应管理
+                //meta: { power:true },
+            },
+            {
+                path: '/system/customermanagement',
+                name: 'customerManagement',
+                component: () => import('../components/content/system/customerManagement'),  //客户管理
+                //meta: { power:true }
+            },
+            {
+                path: '/system/privilegemanagement',
+                name: 'privilegeManagement',
+                component: () => import('../components/content/system/privilegeManagement'),  //权限管理
+                //meta: { power:true }
+            }
+    //    ]
+    //}
+]
 
-export default new Router(constantRouter)
+const createRouter = () => new Router({
+    routes: constantRouter
+})
+const router = createRouter()
+
+export function resetRouter() {
+    const newRouter = createRouter()
+    router.matcher = newRouter.matcher //改变用户登录重新载入路由
+  }
+
+router.addRoutes(asyncRouter)
+
+export default router
