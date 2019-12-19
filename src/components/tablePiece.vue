2b2f8528a208d24f675bc04ddf2a9e49b32ec288
aioverg <1010998682@qq.com>
2019-12-19 20:35:40 +0800

    zujian

---
+++ src/components/tablePiece.vue
@@ -0,0 +1,33 @@
+<template>
+    <table>
+        <caption>{{tableTitle}}</caption>
+        <thead>
+            <tr>
+                <th
+                    v-for="(value, index) in tableHead"
+                    v-bind:key="index"
+                >{{value}}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr
+              v-for="(value, index) in tableBody"
+              v-bind:key="index"
+            >
+                <td>{{index}}</td>
+                <td v-for="(value1, index1) in value" v-bind:key="index1">{{value1}}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: "tablePiece",
+    props: [
+        "tableTitle",
+        "tableHead",
+        "tableBody"
+    ]
+}
+</script>
--- src/views/content/purchase/supplierManagement.vue
+++ src/views/content/purchase/supplierManagement.vue
@@ -6,37 +6,11 @@
       <p>>> 供应商管理</p>
     </div>
     <div id="list-body">
-      <div style="margin: 0 20px 30px 20px;">
-        <div>
-          <p>供应商列表</p>
-          <table>
-            <thead>
-              <tr>
-                <td>序号</td>
-                <td>名称</td>
-                <td>负责人</td>
-                <td>电话</td>
-                <td>手机</td>
-                <td>邮箱</td>
-                <td>地址</td>
-                <td>备注</td>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(value, index) in queryDatas" v-bind:key="index">
-                <td>{{index+1}}</td>
-                <td>{{value[0]}}</td>
-                <td>{{value[1]}}</td>
-                <td>{{value[2]}}</td>
-                <td>{{value[3]}}</td>
-                <td>{{value[4]}}</td>
-                <td>{{value[5]}}</td>
-                <td>{{value[6]}}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <table-piece
+        v-bind:tableTitle="tablePiece.tableTitle"
+        v-bind:tableHead="tablePiece.tableHead"
+        v-bind:tableBody="pageTurn.data"
+      ></table-piece>
     </div>
     <div id="list-foot">
       <p>
@@ -47,8 +21,8 @@
         <button v-on:click="addSupplier">新增</button>
         <span>
           <page-turn
-            v-bind:urlData="url"
-            v-model="queryDatas"
+            v-bind:urlData="pageTurn.url"
+            v-model="pageTurn.data"
           ></page-turn>
         </span>
       </p>
@@ -59,15 +33,27 @@
 <script>
 import purchaseApi from "../../../api/purchase";
 import pageTurn from "../../../components/pageTurn";
+import tablePiece from "../../../components/tablePiece"
 export default {
   name: "supplierManagement",
   components: {
-    "page-turn": pageTurn
+    "page-turn": pageTurn,
+    "table-piece": tablePiece
   },
   data() {
     return {
-      supData: Array, //列表数据
-      url: "/suppliermanagement",
+      pageTurn: {
+        url: "/suppliermanagement",
+        data: null,
+      },
+      tablePiece: {
+        tableTitle: "供应商列表",
+        tableHead: ["序号", "名称", "负责人", "电话", "手机", "邮箱", "地址", "备注"],
+      },
+
+
+
+
       queryData: {
         //axios请求发送的数据
         page: 1, //请求的页面
@@ -75,7 +61,6 @@ export default {
         allPage: null, //总共有多少页数据，每15条算一页
         queryValue: null //查询输入的数据
       },
-      queryDatas: null,
     };
   },
   methods: {
@@ -83,7 +68,7 @@ export default {
     supplierData: function() {
       //获取供应商数据
       purchaseApi.supplierData(this.queryData).then(response => {
-        this.supData = response.data[0]; //获取本次查询的数据
+        this.pageTurn.data = response.data[0]; //获取本次查询的数据
         this.queryData.allPage = Math.ceil(
           response.data[1] / this.queryData.size
         ); //获取服务器中所有符合条件的数据条数，并计算出页数。
